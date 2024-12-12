0264026636ad69327f13636be4b5fe08f3002348
171201 <jinalshah5358@gmail.com>
2024-12-12 21:45:50 +0530

    changes in city component

--- src/context/citiesContext.jsx
+++ src/context/citiesContext.jsx
@@ -86,22 +86,25 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  const getCity = useCallback(async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
 
-      dispatch({ type: "city/loaded", payload: data });
-      //   console.log(data, "context");
-    } catch (err) {
-      dispatch({
-        type: "rejected",
-        payload: "Error while Fetching city data",
-      });
-    }
-  }, []);
+        dispatch({ type: "city/loaded", payload: data });
+        //   console.log(data, "context");
+      } catch (err) {
+        dispatch({
+          type: "rejected",
+          payload: "Error while Fetching city data",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
